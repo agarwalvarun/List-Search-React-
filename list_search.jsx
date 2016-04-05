7c1d01cd3c30e9aee11ba14f113bc29f78819eb0
Varun Agarwal <agar.varun404@gmail.com>
2016-04-06 02:46:19 +0530

    Adde filter-list

--- list_search.jsx
+++ list_search.jsx
@@ -1,12 +1,20 @@
 /** @jsx React.DOM */
 
 var FilteredList = React.createClass({
+	filter-list : function (e) {
+		var initial-list = this.state.initial-items
+		updated-list = initial-list.filter(function(item){
+			return item.toLowerCase().search(e.target.value.toLowerCase()) !== -1
+		})	
+		this.setState({final-items: updated-list})
+	},
 	getInitialState : function(){
 		initial-items = [
 			"Steve Jobs",
 			"Steve Wozniak",
 			"Elon Musk",
 			"Bill Gates",
+			"Mark Zukerberg",
 		]
 		final-items = []
 	},
