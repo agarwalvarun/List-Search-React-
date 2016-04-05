5f27a09bb95673ab00763d0cb450170d6e57829d
Varun Agarwal <agar.varun404@gmail.com>
2016-04-06 02:05:24 +0530

    Made Initial State

--- list_search.jsx
+++ list_search.jsx
@@ -1,1 +1,19 @@
 /** @jsx React.DOM */
+
+var FilteredList = React.createClass({
+	getInitialState : function(){
+		initial-items = [
+			"Steve Jobs",
+			"Steve Wozniak",
+			"Elon Musk",
+			"Bill Gates",
+		]
+		final-items = []
+	},
+	render : function(){
+		<div className = "filter-list">
+			<input type = "text" placeholder = "Search" onChange = {this.filter-list}/>
+			<List final-items = {this.state.final-items}/>
+		</div>
+	}
+}); 
